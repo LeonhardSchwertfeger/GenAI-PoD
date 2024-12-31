cf4ce401c92b43675db3435bb25d1f91ab1e9188
Benjamin Thomas Schwertfeger <development@b-schwertfeger.de>
2024-12-31 10:26:09 +0100

    Resolve "Publishing the documentation using readthedocs.io"

--- doc/conf.py
+++ doc/conf.py
@@ -54,7 +54,6 @@ exclude_patterns = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"  # pylint: disable=invalid-name
-html_static_path = ["_static"]
 html_context = {
     "display_github": True,
     "github_user": "LeonhardSchwertfeger",
